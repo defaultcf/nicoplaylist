a91428daf192d56008b0e2ecbe9db35260ee7040
i544c <leo@i544c.me>
2018-03-05 00:53:18 +0900

    Add style and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,22 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
     ],
   },
