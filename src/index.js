da44aa1eb8fc3adf92c4568e912b8fca2db9b721
i544c <leo@i544c.me>
2018-03-05 01:13:56 +0900

    Add React

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import "./style.css";
 import videoController from './videoController';
 
 videoController();
+
+const body = document.querySelector("body");
+const app_area = document.createElement("nicoplaylist");
+body.appendChild(app_area);
+
+ReactDOM.render(
+  <h1>hello</h1>,
+  app_area
+);
